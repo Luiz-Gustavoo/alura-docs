1c2392ca0cb57da7958b60e5b8e516fc551c5112
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-10-31 20:37:51 -0300

    adicionar funcionalidades

--- public/documento.js
+++ public/documento.js
@@ -1,3 +1,15 @@
-const socket = io();
+// esse arquivo é responsável por manipular o HTML da página documento
 
-socket.emit('mensagem', 'Olá, servidor!');
+import { emitirTextoEditor } from "./socket-front-documento.js";
+
+const textoEditor = document.getElementById("editor-texto");
+
+textoEditor.addEventListener("keyup", () => {
+    emitirTextoEditor(textoEditor.value)
+});
+
+function atualizaTextoEditor(texto) {
+    textoEditor.value = texto;
+}
+
+export {atualizaTextoEditor};
